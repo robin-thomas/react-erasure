aab8d07cbb9f0ac0f334ddeb6c4d40d6b048d987
Robin Thomas <robinthomas2591@gmail.com>
2019-11-30 12:18:17 +0800

    webpack init

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+export { default as Feed } from "./components/feed";
+export { default as Post } from "./components/post";
+export { default as Login } from "./components/login";
+export { default as Stake } from "./components/stake";
+export { default as Reveal } from "./components/reveal";
+export { default as Reward } from "./components/reward";
+export { default as Punish } from "./components/punish";
+export { default as Release } from "./components/release";
+export { default as Retrieve } from "./components/retrieve";
+export { default as Notifications } from "./components/Notifications";
 
-import App from "./app";
-import Notifications from "./components/Notifications";
-
-import DataProvider from "./utils/DataProvider";
-
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-
-const ReactErasure = ({ version = "", griefingType = "countdown" }) => (
-  <DataProvider>
-    <Notifications />
-    <App version={version} griefingType={griefingType}/>
-  </DataProvider>
-);
-
-ReactDOM.render(<ReactErasure />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export { default as ErasureProvider } from "./utils/DataProvider";
--- src/utils/DataProvider.js
+++ src/utils/DataProvider.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
+import ErasureClient from "@robinthomas/erasure-client";
+
 const DataContext = React.createContext();
 
-const DataProvider = props => {
+const DataProvider = ({ version, griefingType, children }) => {
   const [feeds, setFeeds] = useState([]);
   const [alerts, setAlerts] = useState([]);
-  const [client, setClient] = useState(null);
+  const [client, setClient] = useState(new ErasureClient({ version, griefingType }));
   const [disabled, setDisabled] = useState(true);
   const [griefings, setGriefings] = useState([]);
   const [loadingFeeds, setLoadingFeeds] = useState(false);
@@ -23,7 +25,7 @@ const DataProvider = props => {
         loadingGriefings, setLoadingGriefings,
       }}
     >
-      {props.children}
+      {children}
     </DataContext.Provider>
   );
 };
